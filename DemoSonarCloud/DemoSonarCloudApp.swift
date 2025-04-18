86a19891fcbf831936e996c9f12decb45bea669d
Đỗ Lê Duy <duy.do@livepayments.com>
2025-04-18 12:45:23 +0700

    First commit

---
+++ DemoSonarCloud/DemoSonarCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSonarCloudApp.swift
+//  DemoSonarCloud
+//
+//  Created by Đỗ Lê Duy on 18/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoSonarCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoSonarCloudTests/DemoSonarCloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSonarCloudTests.swift
+//  DemoSonarCloudTests
+//
+//  Created by Đỗ Lê Duy on 18/4/25.
+//
+
+import Testing
+@testable import DemoSonarCloud
+
+struct DemoSonarCloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
